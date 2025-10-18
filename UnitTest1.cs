8848b2afceb217d9ec87f67d7bd283a503585fc1
Aman Shaikh <ashaikh34987@gmail.com>
2025-10-18 12:50:21 +0530

    My Second Changes

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,31 +1,21 @@
-﻿namespace MyNewTestProject
+﻿using Microsoft.Playwright;
+
+namespace MyNewTestProject
 {
     [Parallelizable(ParallelScope.Self)]
     [TestFixture]
     public class Tests : PageTest
     {
-       [Test]
-    public void GetStartedLink()
-    {
-        using var playwright = Playwright.CreateAsync().GetAwaiter().GetResult();
-        var browser = playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        [Test]
+        public async Task GetStartedLink()
         {
-            Headless = false
-        }).GetAwaiter().GetResult();
+            await Page.GotoAsync("https://playwright.dev");
 
-        var context = browser.NewContextAsync().GetAwaiter().GetResult();
-        var page = context.NewPageAsync().GetAwaiter().GetResult();
+            // Click the get started link.
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
 
-        page.GotoAsync("https://playwright.dev").GetAwaiter().GetResult();
-
-        // Click the get started link
-        page.GetByRole(AriaRole.Link, new() { Name = "Get started" })
-            .ClickAsync().GetAwaiter().GetResult();
-
-        // Verify Installation heading is visible
-        var heading = page.GetByRole(AriaRole.Heading, new() { Name = "Installation" });
-        
-    }
-
+            // Expects page to have a heading with the name of Installation.
+            await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+        }
     }
 }
